9d206c95665fa6be40f218678a236d734b240f72
nolimitz71 <greninja71@gmail.com>
2025-02-04 19:03:38 -0800

    Added more documentation

--- RegexValidation/src/Main.java
+++ RegexValidation/src/Main.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        
     }
     public static boolean validateCityStateZip(String input) { 
         if (isNullOrEmpty(input)) return false;
